2b275f3529bc154c671a345254afd8b5413d9b2d
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-30 16:01:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,4 +24,3 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.detect {|element| valid_calls == element}
 end
-return element
